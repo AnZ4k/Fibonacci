d340ec18abeb7da66d646feb3229762ce48e8a4e
AnZ4k <77916663+AnZ4k@users.noreply.github.com>
2021-01-25 01:33:53 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,28 +7,28 @@ int seq(int x);
 
 int main()
 {
-		int x;
-		queue<int> result;
-		
-		cout << "Insira um numero: ";
-		cin >> x;
+	int x;
+	queue<int> result;
 
-		for(int i = 1; i <= x; i++)
-		{
-				result.push(seq(i));
-		}
+	cout << "Insira um numero: ";
+	cin >> x;
+
+	for(int i = 1; i <= x; i++)
+	{
+		result.push(seq(i));
+	}
 		
-		while(!result.empty())
-		{
-				cout << result.front() << " ";
-				result.pop();
-		}
-		cout << endl;
+	while(!result.empty())
+	{
+		cout << result.front() << " ";
+		result.pop();
+	}
+	cout << endl;
 
 }
 
 int seq(int x)
 {
-		if (x == 1 || x == 2) return 1;
-		return seq(x - 1) + seq(x - 2);
+	if (x == 1 || x == 2) return 1;
+	return seq(x - 1) + seq(x - 2);
 }
